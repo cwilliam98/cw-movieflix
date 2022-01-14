0552b23f3fc9fbb9640c5d08d73af1f1711e1870
cwilliam98 <cristianwillian174@gmail.com>
2022-01-14 20:24:49 -0300

    Add exceptions

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,13 +2,12 @@ INSERT INTO tb_user (name, email, password) VALUES ('Ana Brown', 'ana@gmail.com'
 INSERT INTO tb_user (name, email, password) VALUES ('Bob Blue', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
 
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 
 INSERT INTO tb_genre (name) VALUES ('Action');
 INSERT INTO tb_genre (name) VALUES ('Comedy');
